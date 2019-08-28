f97698e8e4123c233712b00fd0863829b7d077b0
maksim <maksim.levental@gmail.com>
2019-08-28 16:55:10 -0400

    paths

--- train/run.sh
+++ train/run.sh
@@ -1,1 +1,7 @@
-PYTHONPATH=../ python -m torch.distributed.launch --nproc_per_node=4 train_resnet_dali.py --mx-path=/home/maksim/data/ILSVRC2017_CLS-LOC/ILSVRC/Data/CLS-LOC/imagenet_rec.rec --mx-index-path=/home/maksim/data/ILSVRC2017_CLS-LOC/ILSVRC/Data/CLS-LOC/imagenet_rec.idx
+#!/bin/bash
+export PYTHONPATH=../
+python -m torch.distributed.launch \
+  --nproc_per_node=4 \
+  train_resnet_dali.py \
+  --mx-path=/home/maksim/data/ILSVRC2017_CLS-LOC/ILSVRC/Data/CLS-LOC/imagenet_rec.rec \
+  --mx-index-path=/home/maksim/data/ILSVRC2017_CLS-LOC/ILSVRC/Data/CLS-LOC/imagenet_rec.idx
