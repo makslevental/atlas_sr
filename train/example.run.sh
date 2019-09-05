705a680e39fc70992aeadbf8d74ff40755bed7ce
maksim <maksim.levental@gmail.com>
2019-09-05 14:39:46 -0400

    update training script

--- train/example.run.sh
+++ train/example.run.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 export PYTHONPATH=../
 python -m torch.distributed.launch \
-  --nproc_per_node=1 \
+  --nproc_per_node=4 \
   train_srgan_dali.py \
-  --train-mx-path=/home/maksim/data/ILSVRC2017_CLS-LOC/ILSVRC/Data/CLS-LOC/imagenet_rec.rec \
-  --train-mx-index-path=/home/maksim/data/ILSVRC2017_CLS-LOC/ILSVRC/Data/CLS-LOC/imagenet_rec.idx \
-  --val-mx-path=/home/maksim/data/ILSVRC2017_CLS-LOC/ILSVRC/Data/CLS-LOC/imagenet_val.rec \
-  --val-mx-index-path=/home/maksim/data/ILSVRC2017_CLS-LOC/ILSVRC/Data/CLS-LOC/imagenet_val.idx \
-  --checkpoint-dir=/home/maksim/dev_projects/atlas_sr/checkpoints/srgan2 \
-  --batch-size=16 \
-  --lr=1e-4 \
+  --experiment-name=check_batch_sizes_distributed \
+  --channels=3 \
+  --train-mx-path=/home/maksim/data/VOC2012/voc_train.rec \
+  --train-mx-index-path=/home/maksim/data/VOC2012/voc_train.idx \
+  --val-mx-path=/home/maksim/data/VOC2012/voc_val.rec \
+  --val-mx-index-path=/home/maksim/data/VOC2012/voc_val.idx \
+  --checkpoint-dir=/home/maksim/dev_projects/atlas_sr/checkpoints \
+  --upscale-factor=2 \
+  --epochs=100 \
+  --batch-size=128 \
+  --g-lr=1e-4 \
+  --d-lr=1e-4 \
   --crop-size=88 \
-  --prof
-
+  --workers=4 \
+  --use-apex
