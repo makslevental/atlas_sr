bf3207734fc4e787fb6640cba9c89faae9d3a83f
Maksim Levental <maksim.levental@gmail.com>
2019-09-10 19:18:14 -0400

    add icnr to upsample

--- models/SRGAN.py
+++ models/SRGAN.py
@@ -93,7 +93,47 @@ class ResidualBlock(nn.Module):
         residual = self.bn2(residual)
 
         return x + residual
+    
+    
+def ICNR(tensor, upscale_factor=2, inizializer=nn.init.kaiming_normal):
+    """Fills the input Tensor or Variable with values according to the method
+    described in "Checkerboard artifact free sub-pixel convolution"
+    - Andrew Aitken et al. (2017), this inizialization should be used in the
+    last convolutional layer before a PixelShuffle operation
+    Args:
+        tensor: an n-dimensional torch.Tensor or autograd.Variable
+        upscale_factor: factor to increase spatial resolution by
+        inizializer: inizializer to be used for sub_kernel inizialization
+    Examples:
+        >>> upscale = 8
+        >>> num_classes = 10
+        >>> previous_layer_features = Variable(torch.Tensor(8, 64, 32, 32))
+        >>> conv_shuffle = Conv2d(64, num_classes * (upscale ** 2), 3, padding=1, bias=0)
+        >>> ps = PixelShuffle(upscale)
+        >>> kernel = ICNR(conv_shuffle.weight, scale_factor=upscale)
+        >>> conv_shuffle.weight.data.copy_(kernel)
+        >>> output = ps(conv_shuffle(previous_layer_features))
+        >>> print(output.shape)
+        torch.Size([8, 10, 256, 256])
+    .. _Checkerboard artifact free sub-pixel convolution:
+        https://arxiv.org/abs/1707.02937
+    """
+    new_shape = [int(tensor.shape[0] / (upscale_factor ** 2))] + list(tensor.shape[1:])
+    subkernel = torch.zeros(new_shape)
+    subkernel = inizializer(subkernel)
+    subkernel = subkernel.transpose(0, 1)
 
+    subkernel = subkernel.contiguous().view(subkernel.shape[0],
+                                            subkernel.shape[1], -1)
+
+    kernel = subkernel.repeat(1, 1, upscale_factor ** 2)
+
+    transposed_shape = [tensor.shape[1]] + [tensor.shape[0]] + list(tensor.shape[2:])
+    kernel = kernel.contiguous().view(transposed_shape)
+
+    kernel = kernel.transpose(0, 1)
+
+    return kernel
 
 class UpsampleBLock(nn.Module):
     def __init__(self, in_channels, up_scale):
@@ -101,6 +141,8 @@ class UpsampleBLock(nn.Module):
         self.conv = nn.Conv2d(
             in_channels, in_channels * up_scale ** 2, kernel_size=3, padding=1
         )
+        kernel = ICNR(self.conv.weight, scale_factor=up_scale)
+        self.conv.weight.data.copy_(kernel)
         self.pixel_shuffle = nn.PixelShuffle(up_scale)
         self.prelu = nn.PReLU()
 
