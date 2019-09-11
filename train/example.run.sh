0de761aa4627adda125b6365d3d923d939c56084
maksim <maksim.levental@gmail.com>
2019-09-11 12:29:53 -0400

    load checkpoints

--- train/example.run.sh
+++ train/example.run.sh
@@ -3,18 +3,22 @@ export PYTHONPATH=../
 python -m torch.distributed.launch \
   --nproc_per_node=4 \
   train_srgan_dali.py \
-  --experiment-name=check_batch_sizes_distributed \
+  --experiment-name=srresnet_voc_2x \
+  --checkpoint-dir=~/data/checkpoints \
   --channels=3 \
-  --train-mx-path=/home/maksim/data/VOC2012/voc_train.rec \
-  --train-mx-index-path=/home/maksim/data/VOC2012/voc_train.idx \
-  --val-mx-path=/home/maksim/data/VOC2012/voc_val.rec \
-  --val-mx-index-path=/home/maksim/data/VOC2012/voc_val.idx \
-  --checkpoint-dir=/home/maksim/dev_projects/atlas_sr/checkpoints \
   --upscale-factor=2 \
   --epochs=100 \
   --batch-size=128 \
-  --g-lr=1e-4 \
-  --d-lr=1e-4 \
+  --g-lr=1e-3 \
+  --d-lr=1e-3 \
   --crop-size=88 \
   --workers=4 \
-  --use-apex
+  --print-freq=5 \
+  --use-apex \
+  --train-mx-path=~/data/VOC2012/voc_train.rec \
+  --train-mx-index-path=~/data/VOC2012/voc_train.idx \
+  --val-mx-path=~/data/VOC2012/voc_val.rec \
+  --val-mx-index-path=~/data/VOC2012/voc_val.idx \
+  --tensorboard-dir=~/data/tensorboard
+#  --train-data-dir=~/data/VOC2012/train \
+#  --val-data-dir=~/data/VOC2012/val \
