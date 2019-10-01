c91982d34f54ecf894cab73eaeca8e93e6b29575
maksim <maksim.levental@gmail.com>
2019-10-01 17:16:04 -0400

    working edsr

--- models/edsr.py
+++ models/edsr.py
@@ -1,91 +1,40 @@
-import math
-
-import torch
+import numpy as np
 import torch.nn as nn
+from PIL import Image
+from PIL.Image import BICUBIC
+from matplotlib import pyplot
+from sewar import psnr
+from torchvision.transforms import ToTensor, Resize
 
-from models.common import make_upsample_block
-
-
-class MeanShift(nn.Conv2d):
-    def __init__(
-            self,
-            *,
-            rgb_range=255,
-            rgb_mean=(0.4488, 0.4371, 0.4040),
-            rgb_std=(1.0, 1.0, 1.0),
-            sign=-1,
-    ):
-        super(MeanShift, self).__init__(3, 3, kernel_size=1)
-        std = torch.Tensor(rgb_std)
-        self.weight.data = torch.eye(3).view(3, 3, 1, 1) / std.view(3, 1, 1, 1)
-        self.bias.data = sign * rgb_range * torch.Tensor(rgb_mean) / std
-        for p in self.parameters():
-            p.requires_grad = False
-
-
-class ResBlock(nn.Module):
-    def __init__(self, *, n_feats, kernel_size, res_scale=1):
-        super(ResBlock, self).__init__()
-
-        conv = lambda: nn.Conv2d(
-            in_channels=n_feats,
-            out_channels=n_feats,
-            kernel_size=kernel_size,
-            padding=(kernel_size // 2),
-        )
-
-        self.body = nn.Sequential(conv(), nn.ReLU(), conv())
-        self.res_scale = res_scale
-
-    def forward(self, x):
-        res = self.body(x).mul(self.res_scale)
-        res += x
-
-        return res
+from models.common import default_conv, MeanShift, ResBlock, EDSRUpsampler
+from util.util import load_model_state
 
 
 class EDSR(nn.Module):
-    def __init__(self, scale, *, n_colors=3, n_feats=64, kernel_size=3, n_resblocks=16):
+    def __init__(self, scale, n_resblocks, n_feats, n_colors=3, res_scale=1):
         super(EDSR, self).__init__()
-        self.sub_mean = MeanShift(sign=-1)
+        kernel_size = 3
+        self.sub_mean = MeanShift()
         self.add_mean = MeanShift(sign=1)
 
-        self.head = nn.Sequential(
-            nn.Conv2d(
-                in_channels=n_colors,
-                out_channels=n_feats,
-                kernel_size=kernel_size,
-                padding=(kernel_size // 2),
-            )
-        )
+        # define head module
+        m_head = [default_conv(n_colors, n_feats, kernel_size)]
 
+        # define body module
         m_body = [
-            ResBlock(n_feats=n_feats, kernel_size=kernel_size)
+            ResBlock(n_feats, kernel_size, res_scale=res_scale)
             for _ in range(n_resblocks)
         ]
-        m_body.append(
-            nn.Conv2d(
-                in_channels=n_feats,
-                out_channels=n_feats,
-                kernel_size=kernel_size,
-                padding=(kernel_size // 2),
-            )
-        )
+        m_body.append(default_conv(n_feats, n_feats, kernel_size))
+
+        # define tail module
+        m_tail = [
+            EDSRUpsampler(scale, n_feats),
+            default_conv(n_feats, n_colors, kernel_size),
+        ]
+
+        self.head = nn.Sequential(*m_head)
         self.body = nn.Sequential(*m_body)
-
-        m_tail = []
-        for _ in range(int(math.log2(scale))):
-            conv, pixel_shuffle = make_upsample_block(n_feats)
-            m_tail.extend([conv, pixel_shuffle])
-
-        m_tail.append(
-            nn.Conv2d(
-                in_channels=n_feats,
-                out_channels=n_colors,
-                kernel_size=kernel_size,
-                padding=(kernel_size // 2),
-            )
-        )
         self.tail = nn.Sequential(*m_tail)
 
     def forward(self, x):
@@ -99,3 +48,27 @@ class EDSR(nn.Module):
         x = self.add_mean(x)
 
         return x
+
+
+if __name__ == "__main__":
+    upscale_factor = 2
+    n_resblocks = 32
+    n_feats = 256
+    e = EDSR(upscale_factor, n_resblocks, n_feats, res_scale=0.1)
+    load_model_state(e, "/home/maksim/dev_projects/atlas_sr/train/edsr_2x.pt")
+    image = Image.open(
+        "/home/maksim/data/DSIAC/dsiac_mad_tiffs/cegr01923_0011/420.tiff"
+    )
+    image_np = np.asarray(image)
+    pyplot.imshow(np.asarray(image))
+    pyplot.show()
+
+    w, h = image.size
+    r1 = Resize((h // upscale_factor, w // upscale_factor), interpolation=BICUBIC)
+    image = r1(image)
+    image_tensor = ToTensor()(image)[:3] * 255
+    sr = e(image_tensor.unsqueeze(0)).squeeze(0).permute((1, 2, 0)).data.numpy() / 255
+
+    pyplot.imshow(sr)
+    pyplot.show()
+    print(psnr(image_np[:, :, :3], sr))
